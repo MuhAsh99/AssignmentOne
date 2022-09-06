5ae632e11f94ca33d17bb47dbad89d6db1539561
MuhAsh99 <mjashraf@smu.edu>
2022-09-05 19:43:00 -0500

    Initial Commit

---
+++ AssignmentOne/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AssignmentOne
+//
+//  Created by Muhammad Ashraf on 9/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
