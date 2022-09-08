39d72e94a42e5c3bdfcda247c0f7b4e9c87781a9
MuhAsh99 <mjashraf@smu.edu>
2022-09-08 13:39:31 -0500

    created a selectButton file from a swiftUI view, i commented out the original button stuff for now to see if i can create a button in a better way

---
+++ AssignmentOne/SelectButton.swift
@@ -0,0 +1,20 @@
+//
+//  SelectButton.swift
+//  AssignmentOne
+//
+//  Created by Muhammad Ashraf on 9/8/22.
+//
+
+import SwiftUI
+
+struct SelectButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SelectButton_Previews: PreviewProvider {
+    static var previews: some View {
+        SelectButton()
+    }
+}
--- AssignmentOne/ViewController.swift
+++ AssignmentOne/ViewController.swift
@@ -14,15 +14,15 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         //this is a comment
         
-        let button = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 50))
-          button.backgroundColor = .green
-          button.setTitle("Test Button", for: .normal)
-          button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
-          self.view.addSubview(button)
-
-     }
-        @objc func buttonAction(sender: UIButton!) {
-        print("Button tapped")
-    }
+//        let button = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 50))
+//          button.backgroundColor = .green
+//          button.setTitle("Test Button", for: .normal)
+//          button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+//          self.view.addSubview(button)
+//
+//     }
+//        @objc func buttonAction(sender: UIButton!) {
+//        print("Button tapped")
+//    }
 }
 
