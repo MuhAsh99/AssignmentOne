33d2ead4e79f304c883a1ae2501abd8ef7e0ca1d
MuhAsh99 <mjashraf@smu.edu>
2022-09-08 13:34:58 -0500

    added a button to the view controller, this is for testing purposes right now. I'm going to try to add two buttons after this.

--- AssignmentOne/ViewController.swift
+++ AssignmentOne/ViewController.swift
@@ -13,8 +13,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         //this is a comment
+        
+        let button = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 50))
+          button.backgroundColor = .green
+          button.setTitle("Test Button", for: .normal)
+          button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+          self.view.addSubview(button)
+
+     }
+        @objc func buttonAction(sender: UIButton!) {
+        print("Button tapped")
     }
-
-
 }
 
