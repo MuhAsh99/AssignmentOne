da5718d3c43cc947b8b46b9f1ccb58eee5b62cf8
kasTaghavi <ktaghavi@smu.edu>
2022-09-16 20:19:14 -0500

    Push everything that kas has done to a new branch

--- AssignmentOne/ViewController.swift
+++ AssignmentOne/ViewController.swift
@@ -5,20 +5,45 @@
 //  Created by Muhammad Ashraf on 9/16/22.
 //
 
+
 import UIKit
 
-class StartPage: UIViewController {
-
+class ViewController:UIViewController {
     
     
-    @IBAction func UIButton(_ sender: Any) {
-    }
+    @IBOutlet weak var CitiesPickerDisplay: UITextField!
+    
+    var pickerView = UIPickerView()
+    let cities = ["Dallas","NYC","LA","London","Tokyo"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        pickerView.delegate = self
+        pickerView.dataSource = self
 
-        // Do any additional setup after loading the view.
+        CitiesPickerDisplay.inputView? = pickerView
+        CitiesPickerDisplay.textAlignment = .center
+   
     }
     
-
-
 }
+
+extension ViewController:UIPickerViewDelegate, UIPickerViewDataSource{
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return cities.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return cities[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        CitiesPickerDisplay.text = cities[row]
+        CitiesPickerDisplay.resignFirstResponder()
+    }
+}
+
